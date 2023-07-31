b364beafbe78adcc3d806747168e02ca2df79cea
Esteban Del Cid <estebandelcid.07@gmail.com>
2023-07-31 15:54:12 -0600

    [change] Change Empty component text

--- src/components/EmptyTodos.jsx
+++ src/components/EmptyTodos.jsx
@@ -1,7 +1,7 @@
 const EmptyTodos = () => {
     return (
       <div className=" flex w-[20rem] h-12 justify-center bg-neutral-700 mb-4 p-2 rounded-lg items-center font-bold ">
-        <p className="text-center text-neutral-100">Create your first ToDo!</p>
+        <p className="text-center text-neutral-100">Create the task you need!!</p>
       </div>
     );
 }
