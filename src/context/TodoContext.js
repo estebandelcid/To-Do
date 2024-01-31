4cda210121242784ce9cfcce32d8cb8f11f00675
Esteban Del Cid <estebandelcid.07@gmail.com>
2024-01-30 23:33:28 -0600

    [add] Id to avoid errors with the same text

---
+++ src/context/TodoContext.js
@@ -0,0 +1,69 @@
+import React from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
+
+const TodoContext = React.createContext();
+
+const TodoProvider = ({children}) => {
+  const {
+    item: todos,
+    saveItems: saveTodos,
+    loading,
+    error,
+  } = useLocalStorage("TODOS_V1", []);
+  const [searchValue, setSearchValue] = React.useState("");
+  const [openModal, setOpenModal] = React.useState(false);
+
+  const completedTodos = todos.filter((todo) => !!todo.completed).length;
+  const totalTodos = todos.length;
+
+  const searchedTodos = todos.filter((todo) =>
+    todo.text.toLowerCase().trim().includes(searchValue.toLocaleLowerCase().trim())
+  );
+
+  const completeTodo = (id) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex((todo) => todo.id === id);
+    newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
+
+    return saveTodos(newTodos);
+  };
+
+  const deleteTodo = (id) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex((todo) => todo.id === id);
+    newTodos.splice(todoIndex, 1);
+    return saveTodos(newTodos);
+  };
+  const addTodo = (text) => {
+    const newTodos = [...todos]
+    newTodos.unshift({
+      text,
+      completed: false,
+      id: crypto.randomUUID()
+    })
+    return saveTodos(newTodos)
+  }
+  return (
+  <TodoContext.Provider value={
+    {   
+        totalTodos,
+        completedTodos,
+        searchValue,
+        setSearchValue,
+        searchedTodos,
+        completeTodo,
+        deleteTodo,
+        addTodo,
+        loading,
+        error,
+        openModal,
+        setOpenModal,
+    }
+  }>
+      {children}
+  </TodoContext.Provider>
+  )
+};
+
+
+export { TodoContext, TodoProvider };
