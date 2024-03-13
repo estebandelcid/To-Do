e815ca7160a23df813225c1abf333cdb016dcbc0
Esteban Del Cid <estebandelcid.07@gmail.com>
2024-03-12 23:29:34 -0600

    [fix] same size in loading skeleton and default min height

--- src/components/TodosLoading.jsx
+++ src/components/TodosLoading.jsx
@@ -3,8 +3,6 @@ export const TodosLoading = () => {
     <>
       <div className=" w-11/12 h-12 justify-between bg-white/20 mb-4 p-2 rounded-lg items-center animate-pulse"></div>
       <div className=" w-11/12 h-12 justify-between bg-white/20 mb-4 p-2 rounded-lg items-center animate-pulse"></div>
-      <div className=" w-11/12 h-12 justify-between bg-white/20 mb-4 p-2 rounded-lg items-center animate-pulse"></div>
-      <div className=" w-11/12 h-12 justify-between bg-white/20 mb-4 p-2 rounded-lg items-center animate-pulse"></div>
     </>
   );
 };
