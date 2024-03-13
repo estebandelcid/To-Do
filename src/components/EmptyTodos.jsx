d56272b401d453afbf1a7d11ca5332d2082f390f
Esteban Del Cid <estebandelcid.07@gmail.com>
2024-03-13 01:45:05 -0600

    [fix] typo extra space

--- src/components/EmptyTodos.jsx
+++ src/components/EmptyTodos.jsx
@@ -1,7 +1,7 @@
 export const EmptyTodos = () => {
   return (
     <div className=" flex w-11/12 h-12 justify-center border mb-4 p-2 rounded-lg items-center font-bold ">
-      <p className="text-center text-white">Create the task you need </p>
+      <p className="text-center text-white">Create the task you need</p>
     </div>
   );
 };
