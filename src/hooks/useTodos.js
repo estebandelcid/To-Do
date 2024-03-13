92d03c9932caea929018467df28f4a4810819e4d
Esteban Del Cid <estebandelcid.07@gmail.com>
2024-03-12 23:15:42 -0600

    [change] context replaced by a hook useTodos and delete the context folder

--- src/hooks/useTodos.js
+++ src/hooks/useTodos.js
@@ -1,9 +1,7 @@
 import React from "react";
-import useLocalStorage from "../hooks/useLocalStorage";
+import { useLocalStorage } from "./useLocalStorage";
 
-const TodoContext = React.createContext();
-
-const TodoProvider = ({children}) => {
+export const useTodos = () => {
   const {
     item: todos,
     saveItems: saveTodos,
@@ -11,20 +9,24 @@ const TodoProvider = ({children}) => {
     error,
   } = useLocalStorage("TODOS_V1", []);
   const [searchValue, setSearchValue] = React.useState("");
+
   const [openModal, setOpenModal] = React.useState(false);
 
   const completedTodos = todos.filter((todo) => !!todo.completed).length;
+
   const totalTodos = todos.length;
 
   const searchedTodos = todos.filter((todo) =>
-    todo.text.toLowerCase().trim().includes(searchValue.toLocaleLowerCase().trim())
+    todo.text
+      .toLowerCase()
+      .trim()
+      .includes(searchValue.toLocaleLowerCase().trim())
   );
 
   const completeTodo = (id) => {
     const newTodos = [...todos];
     const todoIndex = newTodos.findIndex((todo) => todo.id === id);
     newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
-
     return saveTodos(newTodos);
   };
 
@@ -35,35 +37,27 @@ const TodoProvider = ({children}) => {
     return saveTodos(newTodos);
   };
   const addTodo = (text) => {
-    const newTodos = [...todos]
+    const newTodos = [...todos];
     newTodos.unshift({
       text,
       completed: false,
-      id: crypto.randomUUID()
-    })
-    return saveTodos(newTodos)
-  }
-  return (
-  <TodoContext.Provider value={
-    {   
-        totalTodos,
-        completedTodos,
-        searchValue,
-        setSearchValue,
-        searchedTodos,
-        completeTodo,
-        deleteTodo,
-        addTodo,
-        loading,
-        error,
-        openModal,
-        setOpenModal,
-    }
-  }>
-      {children}
-  </TodoContext.Provider>
-  )
+      id: crypto.randomUUID(),
+    });
+    return saveTodos(newTodos);
+  };
+
+  return {
+    totalTodos,
+    completedTodos,
+    searchValue,
+    setSearchValue,
+    searchedTodos,
+    completeTodo,
+    deleteTodo,
+    addTodo,
+    loading,
+    error,
+    openModal,
+    setOpenModal,
+  };
 };
-
-
-export { TodoContext, TodoProvider };
