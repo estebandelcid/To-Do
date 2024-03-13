85d9ee43c3dcbd3630b68423d29a76617710f202
Esteban Del Cid <estebandelcid.07@gmail.com>
2024-03-13 01:49:17 -0600

    [change] using render props and new validation for not results matches

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,13 @@
 export const TodoList = (props) => {
   return (
     <ul className="w-11/12 flex flex-col items-center pt-8 pb-4 overscroll-y-contain overflow-y-auto scroll-smooth contenedor rounded-lg">
-      {props.children}
+      {props.loading && props.onLoading()}
+      {props.error && props.onError()}
+      {!props.loading && !props.totalTodos && props.onEmptyTodos()}
+      {!!props.totalTodos &&
+        !props.searchedTodos.length &&
+        props.onEmptySearch(props.searchValue)}
+      {(!props.loading && !props.error) && props.searchedTodos.map(props.render)}
     </ul>
   );
 };
