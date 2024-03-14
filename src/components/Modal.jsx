412fa8272c0249d4a0ba6a0eebf380ca9c9e5103
Esteban Del Cid <estebandelcid.07@gmail.com>
2024-03-14 15:42:52 -0600

    [fix] export type

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDom from 'react-dom'
+import React from "react";
+import ReactDom from "react-dom";
 
-export function Modal ({children}) {
-    return ReactDom.createPortal(
-      <div className="md:hidden w-full fixed flex flex-col justify-center items-center   text-neutral-100 top-0 right-0 left-0 bottom-0 bg-neutral-700 bg-opacity-10 backdrop-blur-3xl z-20">
-        {children}
-      </div>,
-      document.getElementById("modal")
-    );
-}
+export const Modal = ({ children }) => {
+  return ReactDom.createPortal(
+    <div className="md:hidden w-full fixed flex flex-col justify-center items-center   text-neutral-100 top-0 right-0 left-0 bottom-0 bg-neutral-700 bg-opacity-10 backdrop-blur-3xl z-20">
+      {children}
+    </div>,
+    document.getElementById("modal")
+  );
+};
