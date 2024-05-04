8f820be2f470693700a606f3463ea8a183d36105
Esteban Del Cid <estebandelcid.07@gmail.com>
2024-05-04 02:31:42 -0600

    [add]TodoEdit renders the text of the to-dos through a form that has a textarea where the value is the state of 'inputValue' and the edit icon opens the option to edit and when clicked again, through of the editTodo function and using useRef, changes the old text to the new one and focuses the change that was made once after editing.

---
+++ src/components/TodoEdit.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { MdModeEdit } from "react-icons/md";
+import { useTodos } from "../hooks/useTodos";
+
+export const TodoEdit = (props) => {
+  const [disabled, setDisabled] = React.useState(true);
+  const [inputValue, setInputValue] = React.useState(props.text);
+  const focusInputRef = React.useRef();
+  const {
+    stateUpdaters: { editTodo },
+  } = useTodos();
+  const onSubmitUpdate = (event) => {
+    event.preventDefault();
+    const id = props.id;
+    const newText = inputValue;
+    editTodo(id, newText.trim());
+    setDisabled(!disabled);
+    focusInputRef.current.focus();
+  };
+
+  const onChange = (event) => {
+    setInputValue(event.target.value);
+  };
+  return (
+    <form
+      onSubmit={onSubmitUpdate}
+      action=""
+      className="w-full flex items-center"
+    >
+      <textarea
+        className={`box-text w-[95%] text-white bg-transparent text-base break-words pr-8 border-none outline-none  ${
+          props.completed && "line-through opacity-20"
+        }`}
+        value={inputValue}
+        onChange={onChange}
+        readOnly={disabled}
+        ref={focusInputRef}
+        required
+      />
+      <button type="submit">
+        <MdModeEdit
+          size="1.8rem"
+          className="absolute fill-white/70 top-1 right-10"
+        />
+      </button>
+    </form>
+  );
+};
