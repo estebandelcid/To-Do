c4c04818cd658a6067872a06eb1dcfe7d3e20ba0
Esteban Del Cid <estebandelcid.07@gmail.com>
2024-05-04 02:32:30 -0600

    [change] time to loading to-dos

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -25,7 +25,7 @@ export const useLocalStorage = (itemName, initialValue) => {
         setError(true + "Error");
       }
       return () => {};
-    }, 2000);
+    }, 1000);
   }, [synchronizedItem]);
 
   const saveItems = (newItems) => {
