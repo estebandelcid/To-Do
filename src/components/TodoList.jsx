eb129424c549e3da183948c55ec3ed98b115cb75
Esteban Del Cid <estebandelcid.07@gmail.com>
2024-05-04 03:02:55 -0600

    [fix] prevent “bounce” effects

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,6 +1,6 @@
 export const TodoList = (props) => {
   return (
-    <ul className="w-11/12 flex flex-col items-center pt-8 pb-4 overscroll-y-contain overflow-y-auto scroll-smooth contenedor rounded-lg">
+    <ul className="w-11/12 flex flex-col items-center pt-8 pb-4 overscroll-none overflow-y-auto contenedor rounded-lg">
       {props.loading && props.onLoading()}
       {props.error && props.onError()}
       {!props.loading && !props.totalTodos && props.onEmptyTodos()}
